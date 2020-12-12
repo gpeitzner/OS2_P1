6a6344c0e930036bd49f3eae0ab0df0a71ed35ed
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-12-12 11:11:11 -0600

    [fix] kernel modules deploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SO2_P1
+
+sudo apt install build-essential linux-headers-$(uname -r)
--- kernel_modules/processes/deploy.sh
+++ kernel_modules/processes/deploy.sh
@@ -1,6 +1,6 @@
 sudo su
 cd /proc
 rmmod processes
-cd ~/SO2_P1/kernel_modules/processes
+cd /home/ubuntu/SO2_P1/kernel_modules/processes
 make
 insmod processes.ko
