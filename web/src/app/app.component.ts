e4523a0f388cf5e11a4b5dfd0cac615ae29e08b2
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-12-12 23:09:07 -0600

    [add] web design

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'web';
+
+  public chartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+  public chartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40, 33, 58, 97], label: 'RAM' },
+    { data: [28, 48, 40, 19, 86, 27, 90, 23, 11, 68], label: 'CPU' },
+  ];
+  public chartLabels: string[] = [];
+
+  ngOnInit() {
+    setInterval(() => {
+      let date: Date = new Date(Date.now());
+      let currentTime: string =
+        date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+      if (this.chartLabels.length === 0) {
+        this.chartLabels.push(currentTime);
+      } else {
+        if (this.chartLabels.length === 10) {
+          for (let i = 0; i < this.chartLabels.length - 1; i++) {
+            const element = this.chartLabels[i + 1];
+            this.chartLabels[i] = element;
+          }
+          this.chartLabels[9] = currentTime;
+        } else {
+          this.chartLabels.push(currentTime);
+        }
+      }
+    }, 1500);
+  }
 }
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { ChartsModule } from "ng2-charts";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
