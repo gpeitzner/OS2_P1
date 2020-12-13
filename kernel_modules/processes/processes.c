61f97fb09d846732c35c885b726dd1e16714f552
Anthony Chitay <antonchitay@icloud.com>
2020-12-13 12:42:41 -0600

    childs

--- kernel_modules/processes/processes.c
+++ kernel_modules/processes/processes.c
@@ -32,7 +32,7 @@ static int escribir_archivo(struct seq_file * archivo,void *v){
                 }
          }
         }else{
-            seq_printf(archivo, "{ \"padre\": %d, \"user\": %u , \"pid\": %d , \"nombre\": \"%s\" , \"estado\": %ld, \"ram\": 0 },\n",task->pid, task->cred->uid.val, task->pid, task->comm, task->state, 0 );/*    log arent id/executable name/state    */
+            seq_printf(archivo, "{ \"padre\": %d, \"user\": %u , \"pid\": %d , \"nombre\": \"%s\" , \"estado\": %ld, \"ram\": 0 },\n",task->pid, task->cred->uid.val, task->pid, task->comm, task->state );/*    log arent id/executable name/state    */
             list_for_each(list, &task->children){
                 tchild = list_entry( list, struct task_struct, sibling );
                 seq_printf(archivo, "{ \"padre\": %d, \"user\": %u , \"pid\": %d , \"nombre\": \"%s\" , \"estado\": %ld , \"ram\":0 },\n",task->pid,tchild->cred->uid.val, tchild->pid, tchild->comm, tchild->state );
